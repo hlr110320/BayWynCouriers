88802bd7d72813a5d81f278b7a6b3c90ad72bdf1
hlr110320 <110320@gllm.ac.uk>
2022-11-23 21:41:44 +0000

    Added SQL connection and validation for username and password

--- LoginScreen.xaml.cs
+++ LoginScreen.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,38 @@ namespace BayWynCouriersWPF
         public LoginScreen()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection = sqlCon = new SqlConnection(@"Data Source = localhost\?????? Initial Catalog=LoginDB; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    SqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblUser WHERE USERNANME=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is incorrect.")
+                }
+
+            catch (Exception ex)
+            {
+                SqlConnection.Close();
+            }
+
         }
     }
 }
