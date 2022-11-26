0ce918b12b875ce04d04ee549f38daf221eb8f3e
hlr110320 <110320@gllm.ac.uk>
2022-11-26 11:03:06 +0000

    new branch to create seperate methods for event handlers

--- LoginScreen.xaml.cs
+++ LoginScreen.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//BayWynCouriers tracking system
+//Hayley Roberts
+//110320
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -27,18 +31,37 @@ namespace BayWynCouriersWPF
             InitializeComponent();
         }
 
+        //Event handler for clicking the submit button to call the login method
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection sqlCon = new SqlConnection(@"Data Source=mystic-haylo; Initial Catalog = LoginDB; Integrated Security = True");
+            LoginCon();
+        }
+
+        //Event handler for the password box to call the login method when pressing the ENTER key
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            //IF statement checks that the ENTER key is pressed and then calls the method/
+            if (e.Key == Key.Return)
+            {
+               LoginCon();
+            }
+        }
+
+        //The login method for connecting to the Users table and checking login details.
+        void LoginCon()
+        { 
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=mystic-haylo; Initial Catalog = LoginDB; Integrated Security = True");          
             try
             {
                 if (sqlCon.State == System.Data.ConnectionState.Closed)
                     sqlCon.Open();
+
                 String query = "SELECT COUNT(1) FROM tblUsers WHERE Username=@Username AND Password=@Password";
                 SqlCommand sqlCmd = new SqlCommand(query, sqlCon)
                 {
                     CommandType = CommandType.Text
                 };
+
                 sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
                 sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
                 int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
