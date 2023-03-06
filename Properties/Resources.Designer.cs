977d2421a8f8efe00dd2ae3261e132c461212a88
hlr110320 <110320@gllm.ac.uk>
2023-03-06 11:02:46 +0000

    Staged committed

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace BayWynCouriersWinForm.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap delivery_truck {
-            get {
-                object obj = ResourceManager.GetObject("delivery-truck", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
